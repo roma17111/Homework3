930cf82a2354817a6b99fbb486ce2137e27f18d2
Роман <romanze1706@gmail.com>
2022-11-02 21:53:01 +0300

    Проверьте задание 2

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,20 @@ System.out.println("Значение переменной short с типом Ц
 System.out.println("Значение переменной long с типом Целоисчесленные равно " + d);
 System.out.println("Значение переменной float с типом С плавающей точкой равно " + e);
 System.out.println("Значение переменной double с типом С плавающей точкой равно " + f);
+
+float aa = 27.12f;
+long ab = 987678965549L;
+byte ac = 2;
+int ad = 786;
+boolean ae = false;
+short af = -159;
+int ag = 27897;
+byte ah = 1;
+char ai = 33;
+double aj = 3.45678;
+
+
+
+
     }
 }
