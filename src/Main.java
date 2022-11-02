37b4f3fa4df38a9225ed0b83785a7d2ce7e97252
Роман <romanze1706@gmail.com>
2022-11-02 21:17:43 +0300

    Проверьте задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+int a = 2500;
+byte b = 5;
+short c = 3000;
+long d = 5500L;
+float e = 10.5f;
+double f = 365.365;
+System.out.println("Значение переменной int с типом Целоисчесленные равно " + a);
+System.out.println("Значение переменной byte с типом Целоисчесленные равно " + b);
+System.out.println("Значение переменной short с типом Целоисчесленные равно " + c);
+System.out.println("Значение переменной long с типом Целоисчесленные равно " + d);
+System.out.println("Значение переменной float с типом С плавающей точкой равно " + e);
+System.out.println("Значение переменной double с типом С плавающей точкой равно " + f);
     }
 }
